5400198b348a02955cac0d4c9a33b73d659d7125
kruminskr <kruminskr@gmail.com>
2024-11-25 18:37:36 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <div class="wrapper">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
