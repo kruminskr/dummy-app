589ac1863a378c05f6f63d9015d622ee2d5a8ba9
kruminskr <kruminskr@gmail.com>
2024-12-19 14:21:53 +0200

    feat: add views and exeption handling

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import LoginView from '../views/LoginView.vue'
+import AccountView from '../views/AccountView.vue'
+import BalanceView from '../views/BalanceView.vue'
+import TransactionsView from '../views/TransactionsView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login page',
+    component: LoginView,
+  },
+  {
+    path: '/account',
+    name: 'Account page',
+    component: AccountView,
+  },
+  {
+    path: '/account/:id/balance',
+    name: 'Account balance page',
+    component: BalanceView,
+  },
+  {
+    path: '/account/:id/transactions',
+    name: 'Account transactions page',
+    component: TransactionsView,
+  },
+
+  // Route Error handling
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFoundView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes
 })
 
 export default router
