2f2b04c76ca42073e4d86b70f098f591dd35cbec
kruminskr <kruminskr@gmail.com>
2024-12-19 14:22:37 +0200

    feat: add notifications

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-// import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from "vue-toastification";
 
 import App from './App.vue'
 import router from './router'
+import './assets/main.scss'
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App)
 
+const options = {
+    timeout: 3000, 
+    position: "bottom-left",}
+
+app.use(Toast, options);
 app.use(createPinia())
 app.use(router)
 
