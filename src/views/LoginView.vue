c18ac06450f66cafb5e79d2e32aaa7ccf82553d6
kruminskr <kruminskr@gmail.com>
2024-12-19 14:18:42 +0200

    feat: auth view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="columns is-gapless">
+    <!-- Left Side -->
+    <div class="column is-half is-flex is-flex-direction-column is-align-items-center is-justify-content-center">
+      <h1 class="title is-2">Financial managment system</h1>
+      <button class="button is-grey mt-2" @click="redirectSwedbank()">Authenticate</button>
+    </div>
+
+    <!-- Right Side -->
+    <div class="column is-half">
+      <img src="../assets/img/login-bg.jpg" alt="Login Background" class="image is-fullwidth is-fullheight"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const redirectSwedbank = () => {
+  window.location.href = 'http://localhost:3000/api/app/auth';
+}
+</script>
+
+<style scoped>
+.image {
+  object-fit: cover;
+  height: 100%;
+}
+
+.columns {
+  height: 100vh;
+}
+</style>
