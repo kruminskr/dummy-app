66a687e8eb4b63606f099407a456b95f7e066695
kruminskr <kruminskr@gmail.com>
2024-12-19 14:25:26 +0200

    chore: update title and remove unnecesery lines

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
-  <header>
-    <div class="wrapper">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
 </template>
-
